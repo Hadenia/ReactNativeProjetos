85ac3173933238c8918357884e46ca15c6d25bed
hadenia <hadenia@gmail.com>
2025-10-06 23:53:43 -0300

    setimoprojeto

---
+++ setimoprojeto/App.tsx
@@ -0,0 +1,19 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import React from 'react';
+import ProdutoForm from './screens/ProdutoForm';
+import ProdutoLista from './screens/ProdutoLista'
+
+const Stack = createStackNavigator();
+
+export default function App() {
+
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName='ListaProd'>
+                <Stack.Screen name='ListaProd' options={{ title: 'Listagem de Produtos' }} component={ProdutoLista} />
+                <Stack.Screen name='NovoProd' options={{ title: 'Novo Produto' }} component={ProdutoForm} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
